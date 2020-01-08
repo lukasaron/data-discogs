2bdf764e86fc184d47334e641a18b69a10473844
Lukas Aron <lukas.aron@gmail.com>
2020-01-08 15:38:46 +1100

    Comments

--- decode/decoder.go
+++ decode/decoder.go
@@ -8,10 +8,10 @@ import (
 
 // Decoder is the interface that wraps the basic decoding method.
 //
-// Artists, Labels, Masters and Releases are method that parse and decode data from discogs and return the appropriate
+// Artists, Labels, Masters and Releases are method that parse and decode data from Discogs and return the appropriate
 // slice of structured data. When error occurs it is returned as a second parameter.
 //
-// Close method clean all data related to decoding.
+// Close method cleans all data related to decoding.
 type Decoder interface {
 	Decode(write.Writer) error
 	Artists() (int, []model.Artist, error)
@@ -23,10 +23,17 @@ type Decoder interface {
 	Error() error
 }
 
-// Quality Level specifies the required data to be parsed based on the Discogs marking.
+// Quality Level specifies the Quality Data field defined by Discogs in specific order to be able to define
+// basic relationship and provide filtering ability.
 type QualityLevel int
 
-// Quality Level constants defined from a Discogs data.
+// Quality Level constants mirror the Data Quality field defined by Discogs. Quality Level defines the order of these
+// data quality values to be able to filter input items based on the quality level. Moreover, the All value has been
+// added as a basic value (floor level), which covers all quality levels (this is a default value). When another value
+// is set the chosen quality level will be part of decoded data set and all levels above as well. For instance when the
+// value Correct is defined all decoded units with Data Quality level equals to Correct and Complete and Correct will
+// be in the final set. On the other hand each item having Quality Field with value Needs Minor Changes,
+// Needs Major Changes, Needs Vote and Entirely Incorrect will be ignored and will not be part of the result slice.
 const (
 	All QualityLevel = iota
 	EntirelyIncorrect
@@ -77,7 +84,8 @@ type Block struct {
 // File Type determines the input type of data to be decoded.
 type FileType int
 
-// Various types of File types are considered including Unknown type when the type is not specified.
+// Various types of input File types are considered based on the Discogs dump files, moreover this set
+// includes Unknown type for cases when the type is not specified.
 const (
 	Unknown FileType = iota
 	Artists
@@ -88,7 +96,7 @@ const (
 
 // Options consist of QualityLevel, Block settings and FileType that will be decoded.
 type Options struct {
-	QualityLevel QualityLevel
-	Block        Block
-	FileType     FileType
+	QualityLevel QualityLevel // Filters data based on the Data Quality field
+	Block        Block        // Specifies the decoding Block values
+	FileType     FileType     // Identifies XML file type
 }
