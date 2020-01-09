d7833945e1b3a35047bab32c853a6aead300d1c3
Lukas Aron <lukas.aron@gmail.com>
2020-01-10 10:11:28 +1100

    JSONWriter has tests

--- write/json_test.go
+++ write/json_test.go
@@ -1,6 +1,11 @@
 package write
 
-import "testing"
+import (
+	"encoding/json"
+	"github.com/lukasaron/data-discogs/model"
+	"strings"
+	"testing"
+)
 
 func TestJSONWriter_Options(t *testing.T) {
 	j := NewJSONWriter(nil, nil)
@@ -9,4 +14,486 @@ func TestJSONWriter_Options(t *testing.T) {
 	if opt.ExcludeImages {
 		t.Error("exclude images should be false as a default value")
 	}
+}
+
+func TestJSONWriter_WriteArtist(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteArtist(artists[0])
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(artists[0])
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json artist differs from json marshal expected solution")
+	}
+}
+
+func TestJSONWriter_WriteArtists(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteArtists(artists)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(artists)
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json artists differ from json marshal expected solution")
+	}
+}
+
+func TestJSONWriter_WriteLabel(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteLabel(labels[0])
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(labels[0])
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json label differs from json marshal expected solution")
+	}
+}
+
+func TestJSONWriter_WriteLabels(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteLabels(labels)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(labels)
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json labels differ from json marshal expected solution")
+	}
+}
+
+func TestJSONWriter_WriteMaster(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteMaster(masters[0])
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(masters[0])
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json master differs from json marshal expected solution")
+	}
+}
+
+func TestJSONWriter_WriteMasters(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteMasters(masters)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(masters)
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json masters differ from json marshal expected solution")
+	}
+}
+
+func TestJSONWriter_WriteRelease(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteRelease(releases[0])
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(releases[0])
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json release differs from json marshal expected solution")
+	}
+}
+
+func TestJSONWriter_WriteReleases(t *testing.T) {
+	b := &strings.Builder{}
+	j := NewJSONWriter(b, nil)
+	err := j.WriteReleases(releases)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ma, _ := json.Marshal(releases)
+	expected := string(ma)
+	get := b.String()
+	if expected != get {
+		t.Error("json releases differ from json marshal expected solution")
+	}
+}
+
+// ------------------------------------------------------- DATA -------------------------------------------------------
+
+var artists = []model.Artist{
+	{
+		ID:          "2",
+		Name:        "Mr. James Barth & A.D.",
+		RealName:    "Cari Lekebusch & Alexi Delano",
+		DataQuality: "Correct",
+		NameVariations: []string{"Mr Barth & A.D.", "MR JAMES BARTH & A. D.",
+			"Mr. Barth & A.D.", "Mr. James Barth & A. D."},
+		Aliases: []model.Alias{
+			{
+				ID:   "2470",
+				Name: "Puente Latino",
+			},
+			{
+				ID:   "19536",
+				Name: "Yakari & Delano",
+			},
+			{
+				ID:   "103709",
+				Name: "Crushed Insect & The Sick Puppy",
+			},
+			{
+				ID:   "384581",
+				Name: "ADCL",
+			},
+			{
+				ID:   "1779857",
+				Name: "Alexi Delano & Cari Lekebusch",
+			},
+		},
+		Members: []model.Member{
+			{
+				ID:   "26",
+				Name: "Alexi Delano",
+			},
+			{
+				ID:   "27",
+				Name: "Cari Lekebusch",
+			},
+		},
+	},
+}
+
+var labels = []model.Label{
+	{
+		ID:   "1",
+		Name: "Planet E",
+		Images: []model.Image{
+			{
+				Height: "24",
+				Width:  "132",
+				Type:   "primary",
+			},
+			{
+				Height: "126",
+				Width:  "587",
+				Type:   "secondary",
+			},
+			{
+				Height: "196",
+				Width:  "600",
+				Type:   "secondary",
+			},
+			{
+				Height: "121",
+				Width:  "275",
+				Type:   "secondary",
+			},
+			{
+				Height: "720",
+				Width:  "382",
+				Type:   "secondary",
+			},
+			{
+				Height: "398",
+				Width:  "500",
+				Type:   "secondary",
+			},
+			{
+				Height: "189",
+				Width:  "600",
+				Type:   "secondary",
+			},
+		},
+		ContactInfo: "Planet E Communications\r\nP.O. Box 27218\r\nDetroit, Michigan, MI 48227\r\nUSA\r\n\r\n" +
+			"Phone: +1 313 874 8729\r\nFax: +1 313 874 8732\r\nEmail: info@Planet-e.net",
+		Profile: "[a=Carl Craig]'s classic techno label founded in 1991.\r\n\r\n" +
+			"On at least 1 release, Planet E is listed as publisher.",
+		DataQuality: "Correct",
+		Urls: []string{"http://planet-e.net", "http://planetecommunications.bandcamp.com",
+			"http://www.facebook.com/planetedetroit", "http://www.flickr.com/photos/planetedetroit",
+			"http://plus.google.com/100841702106447505236", "http://www.instagram.com/carlcraignet",
+			"http://myspace.com/planetecom", "http://myspace.com/planetedetroit",
+			"http://soundcloud.com/planetedetroit", "http://twitter.com/planetedetroit", "http://vimeo.com/user1265384",
+			"http://en.wikipedia.org/wiki/Planet_E_Communications", "http://www.youtube.com/user/planetedetroit"},
+		SubLabels: []model.LabelLabel{
+			{
+				ID:   "86537",
+				Name: "Antidote (4)",
+			},
+			{
+				ID:   "41841",
+				Name: "Community Projects",
+			},
+			{
+				ID:   "153760",
+				Name: "Guilty Pleasures",
+			},
+			{
+				ID:   "31405",
+				Name: "I Ner Zon Sounds",
+			},
+			{
+				ID:   "277579",
+				Name: "Planet E Communications",
+			},
+			{
+				ID:   "294738",
+				Name: "Planet E Communications, Inc.",
+			},
+			{
+				ID:   "1560615",
+				Name: "Planet E Productions",
+			},
+			{
+				ID:   "488315",
+				Name: "TWPENTY",
+			},
+		},
+	},
+}
+
+var masters = []model.Master{
+	{
+		ID:          "18512",
+		MainRelease: "33699",
+		Images: []model.Image{
+			{
+				Height: "150",
+				Width:  "150",
+				Type:   "primary",
+			},
+			{
+				Height: "592",
+				Width:  "600",
+				Type:   "secondary",
+			},
+			{
+				Height: "592",
+				Width:  "600",
+				Type:   "secondary",
+			},
+		},
+		Artists: []model.ReleaseArtist{
+			{
+				ID:   "212070",
+				Name: "Samuel L Session",
+			},
+		},
+		Genres:      []string{"Electronic"},
+		Styles:      []string{"Tribal", "Techno"},
+		Year:        "2002",
+		Title:       "Psyche EP",
+		DataQuality: "Correct",
+		Videos: []model.Video{
+			{
+				Duration:    "118",
+				Embed:       "true",
+				Src:         "https://www.youtube.com/watch?v=QYf4j0Pd2FU",
+				Title:       "Samuel L. Session - Arrival",
+				Description: "Samuel L. Session - Arrival",
+			},
+			{
+				Duration:    "376",
+				Embed:       "true",
+				Src:         "https://www.youtube.com/watch?v=c_AfLqTdncI",
+				Title:       "Samuel L. Session - Psyche Part 1",
+				Description: "Samuel L. Session - Psyche Part 1",
+			},
+			{
+				Duration:    "419",
+				Embed:       "true",
+				Src:         "https://www.youtube.com/watch?v=0nxvR8Zl9wY",
+				Title:       "Samuel L. Session - Psyche Part 2",
+				Description: "Samuel L. Session - Psyche Part 2",
+			},
+		},
+	},
+}
+
+var releases = []model.Release{
+	{
+		ID:     "2",
+		Status: "Accepted",
+		Images: []model.Image{
+			{
+				Height: "394",
+				Width:  "400",
+				Type:   "primary",
+			},
+			{
+				Height: "600",
+				Width:  "600",
+				Type:   "secondary",
+			},
+			{
+				Height: "600",
+				Width:  "600",
+				Type:   "secondary",
+			},
+		},
+		Artists: []model.ReleaseArtist{
+			{
+				ID:   "2",
+				Name: "Mr. James Barth & A.D.",
+			},
+		},
+		ExtraArtists: []model.ReleaseArtist{
+			{
+				ID:   "26",
+				Name: "Alexi Delano",
+				Role: "Producer, Recorded By",
+			},
+			{
+				ID:   "27",
+				Name: "Cari Lekebusch",
+				Role: "Producer, Recorded By",
+			},
+			{
+				ID:   "26",
+				Name: "Alexi Delano",
+				Anv:  "A. Delano",
+				Role: "Written-By",
+			},
+			{
+				ID:   "27",
+				Name: "Cari Lekebusch",
+				Anv:  "C. Lekebusch",
+				Role: "Written-By",
+			},
+		},
+		Title: "Knockin' Boots Vol 2 Of 2",
+		Formats: []model.Format{
+			{
+				Name:         "Vinyl",
+				Quantity:     "1",
+				Descriptions: []string{"12\"", "33 ⅓ RPM"},
+			},
+		},
+		Genres:      []string{"Electronic"},
+		Styles:      []string{"Broken Beat", "Techno", "Tech House"},
+		Country:     "Sweden",
+		Released:    "1998-06-00",
+		Notes:       "All joints recorded in NYC (Dec.97).",
+		DataQuality: "Correct",
+		MasterID:    "713738",
+		MainRelease: "true",
+		TrackList: []model.Track{
+			{
+				Position: "A1",
+				Title:    "A Sea Apart",
+				Duration: "5:08",
+			},
+			{
+				Position: "A2",
+				Title:    "Dutchmaster",
+				Duration: "4:21",
+			},
+			{
+				Position: "B1",
+				Title:    "Inner City Lullaby",
+				Duration: "4:22",
+			},
+			{
+				Position: "B2",
+				Title:    "Yeah Kid!",
+				Duration: "4:46",
+			},
+		},
+		Identifiers: []model.Identifier{
+			{
+				Description: "Side A Runout Etching",
+				Type:        "Matrix / Runout",
+				Value:       "MPO SK026-A -J.T.S.-",
+			},
+			{
+				Description: "Side B Runout Etching",
+				Type:        "Matrix / Runout",
+				Value:       "MPO SK026-B -J.T.S.-",
+			},
+		},
+		Videos: []model.Video{
+			{
+				Duration:    "310",
+				Embed:       "true",
+				Src:         "https://www.youtube.com/watch?v=MIgQNVhYILA",
+				Title:       "Mr. James Barth & A.D. - A Sea Apart",
+				Description: "Mr. James Barth & A.D. - A Sea Apart",
+			},
+			{
+				Duration:    "265",
+				Embed:       "true",
+				Src:         "https://www.youtube.com/watch?v=LgLchSRehhc",
+				Title:       "Mr. James Barth & A.D. - Dutchmaster",
+				Description: "Mr. James Barth & A.D. - Dutchmaster",
+			},
+			{
+				Duration:    "260",
+				Embed:       "true",
+				Src:         "https://www.youtube.com/watch?v=iaqHaULlqqg",
+				Title:       "Mr. James Barth & A.D. - Inner City Lullaby",
+				Description: "Mr. James Barth & A.D. - Inner City Lullaby",
+			},
+			{
+				Duration:    "290",
+				Embed:       "true",
+				Src:         "https://www.youtube.com/watch?v=x_Os7b-iWKs",
+				Title:       "Mr. James Barth & A.D. - Yeah Kid!",
+				Description: "Mr. James Barth & A.D. - Yeah Kid!",
+			},
+		},
+		Labels: []model.ReleaseLabel{
+			{
+				ID:       "5",
+				Name:     "Svek",
+				Category: "SK 026",
+			},
+			{
+				ID:       "5",
+				Name:     "Svek",
+				Category: "SK026",
+			},
+		},
+		Companies: []model.Company{
+			{
+				ID:             "266169",
+				Name:           "JTS Studios",
+				EntityType:     "29",
+				EntityTypeName: "Mastered At",
+				ResourceURL:    "https://api.discogs.com/labels/266169",
+			},
+			{
+				ID:             "56025",
+				Name:           "MPO",
+				EntityType:     "17",
+				EntityTypeName: "Pressed By",
+				ResourceURL:    "https://api.discogs.com/labels/56025",
+			},
+		},
+	},
 }
